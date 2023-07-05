ca0e0c0f554cc5bfaf2855affcc9f3422c37789d
Daniel <62781672+dwernz@users.noreply.github.com>
2023-07-05 10:45:55 -0400

    added our sign in component and firebase utils

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,59 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCus5SueVLbuju9RQa1pSyrVNffmKTKGw",
+  authDomain: "crwn-db-6ba78.firebaseapp.com",
+  projectId: "crwn-db-6ba78",
+  storageBucket: "crwn-db-6ba78.appspot.com",
+  messagingSenderId: "419589899174",
+  appId: "1:419589899174:web:79fb4624e7a464fc1c4567",
+  measurementId: "G-6BWPL8RXPJ"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
+
